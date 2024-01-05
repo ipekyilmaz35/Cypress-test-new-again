dafb2b26513b67d6f960a6779c4859f009455a4a
ipekyilmaz35 <ipekyi35@gmail.com>
2024-01-05 18:25:21 +0100

    Update of change

--- cypress/e2e/new-test-scenario/new-test-scenario.cy.js
+++ cypress/e2e/new-test-scenario/new-test-scenario.cy.js
@@ -1,17 +1,14 @@
-// google_test.spec.js
-
 describe('SDEP-5657-1 Test Scenario', () => {
-    it('User Opens Google page, types "dog", waits for response, and clicks the first link', () => {
-      // 1. User Opens Google page
-      cy.visit('https://www.google.com')
-  
-      // 2. Type "dog" in google input
-      cy.get('input[name="q"]').type('dog').type('{enter}')
-  
-      // 3. (Waits for response from Google) - Cypress by default waits for the next command to run
-  
-      // 4. Click in the first link provided by Google page
-      cy.get('h3').first().find('a').click()
-    })
+  it('User Opens Google page, types "dog", waits for response, and clicks the first link', () => {
+    // 1. User Opens Google page
+    cy.visit('https://www.google.com')
+
+    // 2. Type "dog" in google input
+    cy.get('input[name="q"]').type('dog').type('{enter}')
+
+    // 3. (Waits for response from Google) - Cypress by default waits for the next command to run
+
+    // 4. Click on the first link provided by Google page. (This code selects the first "a" link inside the "h3" headers to click on the first link from the results of a Google search.)
+    cy.get('h3 a').first().click()
   })
-  
+})
