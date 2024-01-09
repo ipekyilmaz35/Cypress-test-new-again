60533d26133374c86a159a77ebc36c2cc6e2b4f9
ipekyilmaz35 <ipekyi35@gmail.com>
2024-01-09 16:29:23 +0100

    New update after my feedback with Kanchan

--- cypress/e2e/new-test-scenario/new-test-scenario.cy.js
+++ cypress/e2e/new-test-scenario/new-test-scenario.cy.js
@@ -4,11 +4,13 @@ describe('SDEP-5657-1 Test Scenario', () => {
     cy.visit('https://www.google.com')
 
     // 2. Type "dog" in google input
-    cy.get('input[name="q"]').type('dog').type('{enter}')
-
-    // 3. (Waits for response from Google) - Cypress by default waits for the next command to run
+    cy.get('#searchInput').type('dog');
+    cy.get('#searchInput').type('{enter}');
+    
+     // Wait for response from Google. Just example seconds
+     cy.wait(3000) 
 
     // 4. Click on the first link provided by Google page. (This code selects the first "a" link inside the "h3" headers to click on the first link from the results of a Google search.)
-    cy.get('h3 a').first().click()
+    cy.get('h3 > a').first().click()
   })
 })
