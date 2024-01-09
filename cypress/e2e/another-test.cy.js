603468d6699c7db89e5cdd8c6f0631d58fb7422b
ipekyilmaz35 <ipekyi35@gmail.com>
2024-01-09 16:11:53 +0100

    New update aftermy feedback with Kanchan

--- cypress/e2e/another-test.cy.js
+++ cypress/e2e/another-test.cy.js
@@ -1,10 +1,11 @@
 describe('SDEP-5657-2 Test Scenario', () => {
   it('User clicks on the second link provided by Google search', () => {
     cy.visit('https://www.google.com')
-    cy.get('input[name="q"]').type('dog').type('{enter}')
+    cy.get('#searchInput').type('dog');
+    cy.get('#searchInput').type('{enter}');
     
     // Wait for response from Google. Just example seconds
-    cy.wait(2000) 
+    cy.wait(3000) 
 
     // This code selects the second "a" link inside the "h3" headers to click on the second link from the results of the search on Google.
     cy.get('h3 a').eq(1).click()
