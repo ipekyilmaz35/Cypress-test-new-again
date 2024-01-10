b81c92ad8b09cfacb922a9220bbf6f283b559408
ipekyilmaz35 <ipekyi35@gmail.com>
2024-01-10 15:19:33 +0100

    Second update after Kanchan's feedback.

--- cypress/e2e/another-test.cy.js
+++ cypress/e2e/another-test.cy.js
@@ -1,13 +1,19 @@
 describe('SDEP-5657-2 Test Scenario', () => {
   it('User clicks on the second link provided by Google search', () => {
+    // 1. User Opens Google page
     cy.visit('https://www.google.com')
-    cy.get('#searchInput').type('dog');
-    cy.get('#searchInput').type('{enter}');
     
-    // Wait for response from Google. Just example seconds
-    cy.wait(3000) 
+    // 2. Type "dog" in google input
+    cy.visit('https://www.google.com');
+    // As Kanchan recommended, I typed here in 2 lines below:
+    cy.get('#APjFqb').type('dog');
+    cy.get('#APjFqb').type('{enter}');
 
-    // This code selects the second "a" link inside the "h3" headers to click on the second link from the results of the search on Google.
-    cy.get('h3 a').eq(1).click()
+     // 3. Wait some seconds for search results to load
+     cy.wait(10000); 
+
+    // 4. Click on the second link provided by Google page. 
+    // As Iga recommended I used Cypress locator feature in here.
+    cy.get(':nth-child(3) > .g > .srKDX > .jGGQ5e > .yuRUbf > :nth-child(1) > [jscontroller="msmzHf"] > a > .LC20lb').click()
   })
 })
