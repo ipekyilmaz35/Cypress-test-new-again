1e3f0914632d5c4dab3c7e1ba10fe01055c5b702
ipekyilmaz35 <ipekyi35@gmail.com>
2024-01-11 13:45:03 +0100

    -

--- cypress/e2e/another-test.cy.js
+++ cypress/e2e/another-test.cy.js
@@ -2,12 +2,14 @@ describe('SDEP-5657-2 Test Scenario', () => {
   it('User clicks on the second link provided by Google search', () => {
     // 1. User Opens Google page
     cy.visit('https://www.google.com')
+
+    cy.get('#L2AGLb > div').click
     
     // 2. Type "dog" in google input
     cy.visit('https://www.google.com');
     // As Kanchan recommended, I typed here in 2 lines below:
     cy.get('#APjFqb').type('dog');
-    cy.get('#APjFqb').type('{enter}');
+    cy.get('button').type('{enter}');
 
      // 3. Wait some seconds for search results to load
      cy.wait(10000); 
