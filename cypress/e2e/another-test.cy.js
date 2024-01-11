d1dd2fbfafe314b6bd11da26b39d774a707618e2
ipekyilmaz35 <ipekyi35@gmail.com>
2024-01-11 14:29:21 +0100

    commit for the last changes

--- cypress/e2e/another-test.cy.js
+++ cypress/e2e/another-test.cy.js
@@ -3,16 +3,15 @@ describe('SDEP-5657-2 Test Scenario', () => {
     // 1. User Opens Google page
     cy.visit('https://www.google.com')
 
-    cy.get('#L2AGLb > div').click
+    //Google terms:
+    cy.get('#L2AGLb > div').click();
     
     // 2. Type "dog" in google input
     cy.visit('https://www.google.com');
-    // As Kanchan recommended, I typed here in 2 lines below:
-    cy.get('#APjFqb').type('dog');
-    cy.get('button').type('{enter}');
+    cy.get('#APjFqb').type('dog').type('{enter}');
 
      // 3. Wait some seconds for search results to load
-     cy.wait(10000); 
+     cy.wait(15000); 
 
     // 4. Click on the second link provided by Google page. 
     // As Iga recommended I used Cypress locator feature in here.
