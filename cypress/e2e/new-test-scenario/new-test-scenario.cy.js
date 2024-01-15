3ae8f3cc189ad307d94428f601fc7bdc48f2b985
ipekyilmaz35 <ipekyi35@gmail.com>
2024-01-15 11:27:24 +0100

    Commit after some changes - 15/01/2024

--- cypress/e2e/new-test-scenario/new-test-scenario.cy.js
+++ cypress/e2e/new-test-scenario/new-test-scenario.cy.js
@@ -2,7 +2,7 @@ describe('SDEP-5657-1 Test Scenario', () => {
   it('User Opens Google page, types "dog", waits for response, and clicks the first link', () => {
 
     // 1. User Opens Google page
-    cy.visit('https://www.google.com')
+    cy.visit('https://www.google.com');
 
     //Google terms:
     cy.get('#L2AGLb > div').click();
@@ -11,12 +11,11 @@ describe('SDEP-5657-1 Test Scenario', () => {
     cy.visit('https://www.google.com');
     cy.get('#APjFqb').type('dog').type('{enter}');
     
-     // Wait for response from Google. Just example seconds
-     cy.wait(15000) 
+    // Wait for response from Google. Just example seconds
+    cy.wait(3000) 
 
-    // 4. Click on the first link provided by Google page. 
-    // As Iga recommended I used Cypress locator feature in here.
-    cy.get(':nth-child(1) > .MjjYud > .g > .srKDX > .jGGQ5e > .yuRUbf > :nth-child(1) > [jscontroller="msmzHf"] > a > .LC20lb').click();
+    // 4. Click on the first link provided by Google page. (Iga's recommendation)
+    cy.get('h3').eq(1).click({force: true});
   })
 })
 
